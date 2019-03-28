67e17766d168f90f51ec373a87174b870939d3ef
asdredmitry <michalych2014@yandex.ru>
2019-03-28 13:50:14 +0300

    Create instance and info structures

--- vulkanTest.cpp
+++ vulkanTest.cpp
@@ -10,6 +10,9 @@ const int HEIGHT = 200;
 
 class HelloTriangleApplication
 {
+    private:
+        GLFWwindow * window;
+        VkInstance instance;
     public:
         void run()
         {
@@ -19,9 +22,9 @@ class HelloTriangleApplication
             cleanup();
         }
     private:
-        GLFWwindow * window;
         void initVulkan()
         {
+            createInstance();
 
         }
         
@@ -46,6 +49,36 @@ class HelloTriangleApplication
             glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
             glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
             window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        }
+        void createInstance()
+        {
+            VkApplicationInfo appInfo = {};
+            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            appInfo.pApplicationName = "Hello Triangle";
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
+
+            VkInstanceCreateInfo createInfo = {};
+            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+            createInfo.pApplicationInfo = &appInfo;
+
+            uint32_t glfwExtensionCount = 0;
+            const char ** glfwExtensions;
+
+            glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            createInfo.enabledExtensionCount = glfwExtensionCount;
+            createInfo.ppEnabledExtensionNames = glfwExtensions;
+            createInfo.enabledLayerCount = 0;
+            
+            VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+            if(result != VK_SUCCESS)
+            {
+                throw std :: runtime_error("failed to create instance!");
+            }
         }
 };
 int main()
